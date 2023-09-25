d5e6979a79ca0b88d74a578dc3d0e5ce5629c029
Ruwini Thakshila <ruwinithakshila2017@gmail.com>
2023-09-25 18:36:23 +0000

    Add User Class

--- app/shared/src/main/java/lk/ijse/dep11/app/controller/ServerViewController.java
+++ app/shared/src/main/java/lk/ijse/dep11/app/controller/ServerViewController.java
@@ -1,4 +1,20 @@
 package lk.ijse.dep11.app.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep11.app.User;
+
 public class ServerViewController {
+    public ListView<String> lvChat;
+    public ListView<User> lvPeople;
+    public Button btnSend;
+    public TextField txtMessage;
+    public AnchorPane root;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+
+    }
 }
